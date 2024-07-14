df1d70aac570f1cbc870a67c0ff9230a0ca9593a
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-14 11:16:53 +0300

    Add mapRange util fn

--- src/shared/utils/utils.tsx
+++ src/shared/utils/utils.tsx
@@ -1,6 +1,23 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+// Styles
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Math
+
+export const mapRange = (
+  inputLower: number,
+  inputUpper: number,
+  outputLower: number,
+  outputUpper: number,
+) => {
+  const inputRange = inputUpper - inputLower;
+  const outputRange = outputUpper - outputLower;
+
+  return (value: number) =>
+    outputLower + (((value - inputLower) * inputRange) / outputRange || 0);
+};
