efae7fe9fae14e4980a9243a492f958dc1eeca01
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-16 23:17:33 +0300

    Add Hero section to the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import { HeroImage } from './_ui/hero-image';
+import { ScrollTitle } from './_ui/scroll-title';
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <main>
+      <section className="relative flex h-without-header items-center overflow-hidden">
+        <div className="absolute bottom-0 z-10">
+          <ScrollTitle />
+        </div>
+        <div className="flex flex-1 justify-end">
+          <h2 className="flex flex-col pb-40 pe-7/12 ps-10 text-4xl font-bold">
+            <span>Freelance</span>
+            <span>Front-End Developer</span>
+          </h2>
+        </div>
+        <HeroImage className="absolute right-0 top-0 h-without-header w-7/12" />
+      </section>
+    </main>
   );
 }
