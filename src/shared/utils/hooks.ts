33874aa74875f63d0d3ebabb4b133001fc5e333e
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-16 23:18:07 +0300

    Create useViewport util hook

--- src/shared/utils/hooks.ts
+++ src/shared/utils/hooks.ts
@@ -11,3 +11,30 @@ export const useIsClient = () => {
 
   return isClient;
 };
+
+export interface Viewport {
+  width: number;
+  height: number;
+}
+
+const detectViewport = (): Viewport => ({
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+
+export const useViewport = (): Viewport  => {
+  const [viewport, setViewport] = useState<Viewport>({width: 0, height: 0});
+
+  useEffect(() => {
+    setViewport(detectViewport);
+
+    const handleResize = () => {
+      setViewport(detectViewport);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return viewport;
+};
